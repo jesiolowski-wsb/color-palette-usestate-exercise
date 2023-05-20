82f260febc01c50140b29132a68dea6d48ee8d0b
filip.jesiolowski.sp <filip.jesiolowski.sp@lhsystems.com>
2023-05-20 16:41:07 +0200

    initial commit

---
+++ src/components/colorpalette.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+const ColorPalette = () => {
+    // Declare a state variable called 'colors' and its setter function 'setColors'
+    const [colors, setColors] = useState(["#FF0000", "#00FF00", "#0000FF"]);
+
+    // Declare a state variable called 'newColor' and its setter function 'setNewColor'
+    const [newColor, setNewColor] = useState("");
+
+    // Function to handle adding a new color to the palette
+    const addColor = () => {
+        if (newColor) {
+            setColors([...colors, newColor]);
+            setNewColor("");
+        }
+    };
+
+    // Function to handle removing a color from the palette
+    const removeColor = (index) => {
+        const updatedColors = [...colors];
+        updatedColors.splice(index, 1);
+        setColors(updatedColors);
+    };
+
+    return (
+        <div>
+            <h2>Color Palette</h2>
+            <input
+                type="color"
+                value={newColor}
+                onChange={(e) => setNewColor(e.target.value)}
+                placeholder="Enter a color"
+            />
+            <button onClick={addColor}>Add Color</button>
+            <p>Total Colors: {colors.length}</p>
+            <div className="color-container">
+                {colors.map((color, index) => (
+                    <div
+                        key={index}
+                        className="color"
+                        style={{
+                            backgroundColor: color,
+                            border: "1px solid black",
+                            height: "100px",
+                        }}
+                        onClick={() => removeColor(index)}
+                    ></div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default ColorPalette;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ColorPalette from "./components/colorpalette";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ColorPalette/>
   </React.StrictMode>
 );
 
